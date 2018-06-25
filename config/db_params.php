8e6b87a3da72f4b04d64da543d38a3ff57fdf60a
svarowskey <vladimir.svarowski@gmail.com>
2018-06-25 13:27:05 +0600

    Added changes in a config file for connection database

--- config/db_params.php
+++ config/db_params.php
@@ -3,7 +3,7 @@
     return array(
         //Объявляем переменные и назначаем данные для подключения к БД
         'host'      => 'localhost',
-        'dbname'    => 'super_mag',
+        'dbname'    => 'vladimir-svarows_super-mag',
         'user'      => 'root',
         'password'  => '',
     );
